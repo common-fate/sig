0916331bd6a3caf16d99434330e4d58148ac8b3e
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2022-04-01 15:11:12 +0100

    add EnrollAccessHandler

--- enroll_access_handler_test.go
+++ enroll_access_handler_test.go
@@ -1,82 +1,17 @@
 package sig
 
 import (
-	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"crypto/x509"
 	"math/big"
 	"testing"
 	"time"
 )
 
-func TestBuildAssumeRoleDigest(t *testing.T) {
-	type test struct {
-		input AssumeAwsIamRequest
-		valid bool
-	}
-
-	tests := []test{
-		{
-			// this one hashes to the digest that is hardcoded in the test
-			input: AssumeAwsIamRequest{
-
-				Account: "12345",
-				AssumeRequest: AssumeRequest{
-					Role:            "test",
-					CertFingerprint: [32]byte{0, 10, 20, 30},
-					TimeNanos:       time.Unix(10, 10).UnixNano(),
-				},
-			},
-			valid: true,
-		},
-		// other inputs should produce different hashes
-		{
-			input: AssumeAwsIamRequest{
-
-				Account: "12345",
-				AssumeRequest: AssumeRequest{
-					Role:            "test",
-					CertFingerprint: [32]byte{0, 10, 20, 30},
-					TimeNanos:       time.Unix(10, 11).UnixNano()},
-			},
-			valid: false,
-		},
-		{
-			input: AssumeAwsIamRequest{
-
-				Account: "12345",
-				AssumeRequest: AssumeRequest{
-					Role:            "test",
-					CertFingerprint: [32]byte{0, 10, 20},
-					TimeNanos:       time.Unix(10, 10).UnixNano(),
-				},
-			},
-			valid: false,
-		},
-	}
-
-	expected := []byte{225, 188, 39, 220, 161, 200, 127, 42, 118, 123, 96, 37, 216, 191, 206, 24, 121, 208, 175, 78, 36, 223, 159, 152, 148, 35, 231, 239, 252, 169, 22, 143}
-
-	for _, tc := range tests {
-		digest, err := tc.input.Digest()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if tc.valid {
-			if !bytes.Equal(digest, expected) {
-				t.Fatalf("expected hashes to match: %+v, got: %+v", expected, digest)
-			}
-		} else {
-			if bytes.Equal(digest, expected) {
-				t.Fatalf("expected hashes to be different: %+v, got: %+v", expected, digest)
-			}
-		}
-	}
-}
-
-func TestVerifyAssumeAwsIamRequest(t *testing.T) {
+func TestVerifyEnrollAccessHandlerRequest(t *testing.T) {
 	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
 		t.Fatal(err)
@@ -92,6 +27,16 @@ func TestVerifyAssumeAwsIamRequest(t *testing.T) {
 	}
 
 	cert, err := x509.ParseCertificate(der)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	der, err = x509.CreateCertificate(rand.Reader, &tmpl, &tmpl, &priv.PublicKey, priv)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	invalidCertWithValidPrivkey, err := x509.ParseCertificate(der)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -127,13 +72,14 @@ func TestVerifyAssumeAwsIamRequest(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	req := AssumeAwsIamRequest{
-		Account: "12345",
-		AssumeRequest: AssumeRequest{
-			Role:            "test",
-			CertFingerprint: [32]byte{0, 10, 20, 30},
-			TimeNanos:       time.Unix(10, 10).UnixNano(),
-		},
+	fingerprint := sha256.Sum256(cert.Raw)
+
+	req := EnrollAccessHandlerRequest{
+		Token:            "12345",
+		ProviderID:       "aws",
+		AccessHandlerURL: "http://test.com",
+		CertFingerprint:  fingerprint,
+		TimeNanos:        time.Unix(10, 10).UnixNano(),
 	}
 
 	type test struct {
@@ -151,28 +97,23 @@ func TestVerifyAssumeAwsIamRequest(t *testing.T) {
 		"certificate expired":           {useInvalidData: false, time: time.Unix(10, 10).Add(time.Hour * 10), cert: cert, err: &ErrInvalidSignature{Reason: "certificate already expired at 1970-01-01T00:00:12Z"}},
 		"certificate missing NotBefore": {useInvalidData: false, time: time.Unix(10, 10).Add(time.Hour * 10), cert: missingInforCert, err: &ErrInvalidSignature{Reason: "certificate does not have a validity interval specified"}},
 		"certificate missing NotAfter":  {useInvalidData: false, time: time.Unix(10, 10).Add(time.Hour * 10), cert: missingInforCert, err: &ErrInvalidSignature{Reason: "certificate does not have a validity interval specified"}},
-		"signature in future":           {useInvalidData: false, time: time.Unix(0, 0), cert: othercert, err: &ErrInvalidSignature{Reason: "payload time 1970-01-01T00:00:10Z is in the future"}},
-		"signature too far in past":     {useInvalidData: false, time: time.Unix(10, 10).Add(time.Hour * 10), cert: othercert, err: &ErrInvalidSignature{Reason: "payload time 1970-01-01T00:00:10Z is too old"}},
+		"signature in future":           {useInvalidData: false, time: time.Unix(0, 0), cert: othercert, err: &ErrInvalidSignature{Reason: "signature time 1970-01-01T00:00:10Z is in the future"}},
+		"signature too far in past":     {useInvalidData: false, time: time.Unix(10, 10).Add(time.Hour * 10), cert: othercert, err: &ErrInvalidSignature{Reason: "signature time 1970-01-01T00:00:10Z is too old"}},
+		"invalid fingerprint":           {useInvalidData: false, time: time.Unix(11, 10), cert: invalidCertWithValidPrivkey, err: &ErrInvalidSignature{Reason: "certificate fingerprint did not match payload"}},
 	}
 
 	for name, tc := range tests {
-
 		sig, err := req.Sign(priv)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		signed := &SignedAssumeAwsIamRequest{
-			AssumeAwsIamRequest: req,
-			Sig:                 sig,
-		}
-
 		if tc.useInvalidData {
 			// change the data so the payload being verified is different
-			signed.AssumeAwsIamRequest.Account = "different"
+			req.ProviderID = "different"
 		}
 
-		err = Valid(signed, tc.cert, WithVerificationTime(tc.time, 5*time.Minute))
+		err = req.Valid(sig, tc.cert, WithVerificationTime(tc.time, 5*time.Minute))
 		if tc.err == nil && err != nil {
 			t.Fatalf("%s: expected no error but got: %+v", name, err)
 		} else if tc.err != nil && err != nil {
---
+++ enroll_acess_handler.go
@@ -0,0 +1,125 @@
+package sig
+
+import (
+	"bytes"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/x509"
+	"fmt"
+	"time"
+
+	sigv1alpha1 "github.com/common-fate/sig/sig/v1alpha1"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type EnrollAccessHandlerRequest struct {
+	Token            string   `json:"token"`
+	ProviderID       string   `json:"providerId"`
+	AccessHandlerURL string   `json:"accessHandlerUrl"`
+	CertFingerprint  [32]byte `json:"cert"`
+	TimeNanos        int64    `json:"time"`
+}
+
+// Digest builds the canonical digest of the assume role
+// payload which can be signed and verified.
+func (a *EnrollAccessHandlerRequest) Digest() ([]byte, error) {
+	p1 := sigv1alpha1.EnrollAccessHandlerSignature{
+		Token:                  a.Token,
+		ProviderId:             a.ProviderID,
+		AccessHandlerUrl:       a.AccessHandlerURL,
+		Timestamp:              timestamppb.New(time.Unix(0, a.TimeNanos)),
+		CertificateFingerprint: a.CertFingerprint[:],
+	}
+	msg, err := proto.Marshal(&p1)
+	if err != nil {
+		return nil, err
+	}
+	hash := sha256.Sum256(msg)
+
+	return hash[:], nil
+}
+
+// Sign an AssumeRole request.
+func (a *EnrollAccessHandlerRequest) Sign(s crypto.Signer) ([]byte, error) {
+	digest, err := a.Digest()
+	if err != nil {
+		return nil, err
+	}
+	return s.Sign(rand.Reader, digest, crypto.SHA256)
+}
+
+// Valid verifies that a signature is valid. It performs the following checks:
+//
+// 1. Is the payload signature provided valid for the certificate?
+//
+// 2. Is the timestamp in the payload valid?
+//
+// 3. Does the certificate match the fingerprint provided in the payload?
+//
+// Timestamps are considered valid if they have occurred up to 5 minutes before time.Now().
+// sig.WithVerificationTime() can be passed to customise this.
+func (a *EnrollAccessHandlerRequest) Valid(sig []byte, cert *x509.Certificate, opts ...func(*verifyConf)) error {
+	cfg := &verifyConf{
+		Now:             time.Now(),
+		AllowedDuration: time.Minute * 5,
+	}
+	for _, o := range opts {
+		o(cfg)
+	}
+
+	// certificates must have NotBefore and NotAfter specified
+	var zeroTime time.Time
+	if cert.NotAfter.Equal(zeroTime) || cert.NotBefore.Equal(zeroTime) {
+		return &ErrInvalidSignature{Reason: "certificate does not have a validity interval specified"}
+	}
+
+	// certificates MUST be valid before a request is made
+	certificateNotYetValid := cert.NotBefore.After(cfg.Now)
+	if certificateNotYetValid {
+		return &ErrInvalidSignature{Reason: fmt.Sprintf("certificate is not valid until %s", cert.NotBefore.UTC().Format(time.RFC3339))}
+	}
+
+	// certificates can have expired within the AllowedDuration
+	certificateExpired := cert.NotAfter.Before(cfg.Now.Add(-cfg.AllowedDuration))
+	if certificateExpired {
+		return &ErrInvalidSignature{Reason: fmt.Sprintf("certificate already expired at %s", cert.NotAfter.UTC().Format(time.RFC3339))}
+	}
+
+	digest, err := a.Digest()
+	if err != nil {
+		return &ErrInvalidSignature{Reason: fmt.Sprintf("error building digest: %s", err.Error())}
+	}
+	key, ok := cert.PublicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return &ErrInvalidSignature{Reason: "certificate did not have ECDSA signing key"}
+	}
+
+	plTime := time.Unix(0, a.TimeNanos)
+
+	// check the timestamp in the payload matches
+	if plTime.After(cfg.Now) {
+		return &ErrInvalidSignature{Reason: fmt.Sprintf("signature time %s is in the future", plTime.UTC().Format(time.RFC3339))}
+	}
+	if plTime.Before(cfg.Now.Add(-cfg.AllowedDuration)) {
+		return &ErrInvalidSignature{Reason: fmt.Sprintf("signature time %s is too old", plTime.UTC().Format(time.RFC3339))}
+	}
+
+	valid := ecdsa.VerifyASN1(key, digest, sig)
+	if !valid {
+		return &ErrInvalidSignature{Reason: "signature is invalid"}
+	}
+
+	// data verification:
+	// verify that the fingerprint of the signing certificate matches the fingerprint provided in the payload
+
+	fingerprint := sha256.Sum256(cert.Raw)
+	if !bytes.Equal(fingerprint[:], a.CertFingerprint[:]) {
+		return &ErrInvalidSignature{Reason: "certificate fingerprint did not match payload"}
+	}
+
+	return nil
+}
